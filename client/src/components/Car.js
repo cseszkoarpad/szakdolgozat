2b4e3dad54dbf031945dce0e100d3b7df0e866cf
Cseszkó Árpád <cseszko.arpad@gmail.com>
2018-07-27 23:20:10 +0200

    grid list added

--- client/src/components/Car.js
+++ client/src/components/Car.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import GridListTile from '@material-ui/core/es/GridListTile/GridListTile';
+import GridListTileBar from '@material-ui/core/es/GridListTileBar/GridListTileBar';
+import IconButton from '@material-ui/core/es/IconButton/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
-const Car = ({kep, marka, modell, _id}) => {
-  return (
+const Car = ({kep, marka, modell, _id}) => (
+  <GridListTile key={_id}>
     <Link className="car" to={`/cars/${_id}`}>
-      <img className="img-car"
-           src={kep ? kep : 'http://maestroselectronics.com/wp-content/uploads/2017/12/No_Image_Available.jpg'}
+      <img src={kep ? kep : 'http://maestroselectronics.com/wp-content/uploads/2017/12/No_Image_Available.jpg'}
            alt={`${marka}-${modell}`}/>
-      <h6 className="title-car">{marka} - {modell}</h6>
+      <GridListTileBar
+        title={marka}
+        subtitle={<span>{modell}</span>}
+        actionIcon={
+          <IconButton>
+            <InfoIcon/>
+          </IconButton>
+        }
+      />
     </Link>
-  );
-};
+  </GridListTile>
+);
 
 export default Car;
--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,41 +1,88 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import Car from '../components/Car';
 import Loader from '../components/Loader';
 import Search from '../components/Search';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import {Link} from 'react-router-dom';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+
+const styles = {
+  search: {
+    padding: '10px',
+  },
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'space-around',
+    justifyContent: 'space-around',
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+};
 
 class Main extends Component {
-  renderCars = () => {
-    return this.props.cars.map(car => <Car key={car._id} {...car} />);
-  };
+  renderCars = (classes) => this.props.cars.map(car => (
+      <GridListTile key={car._id} style={{width: '150px', height: '168px'}}>
+        <Link className="car" to={`/cars/${car._id}`}>
+          <img
+            src={car.kep ? car.kep : 'http://maestroselectronics.com/wp-content/uploads/2017/12/No_Image_Available.jpg'}
+            alt={`${car.marka}-${car.modell}`}/>
+          <GridListTileBar
+            title={car.marka}
+            subtitle={<span>{car.modell}</span>}
+            actionIcon={
+              <IconButton className={classes.icon}>
+                <InfoIcon/>
+              </IconButton>
+            }
+          />
+        </Link>
+      </GridListTile>
+    ),
+  );
 
   render() {
+    const {classes} = this.props;
+
     if (this.props.cars) {
       return (
         <Grid container spacing={8}>
           <Grid item xs={2}>
-            <Paper className="search">
+            <Paper className={classes.search}>
               <Search/>
             </Paper>
           </Grid>
           <Grid item xs={10}>
-            <Paper className="cars">{this.renderCars()}</Paper>
+            <Paper>
+              <GridList className={classes.root}>
+                {this.renderCars(classes)}
+              </GridList>
+            </Paper>
           </Grid>
         </Grid>
       );
     } else {
       return <Loader/>;
     }
-
   }
 }
 
+Main.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
 function mapStateToProps({cars}) {
   return {
-    cars
+    cars,
   };
 }
 
-export default connect(mapStateToProps)(Main);
+export default connect(mapStateToProps)(withStyles(styles)(Main));
